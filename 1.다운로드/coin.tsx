79b1932b5d2587ce6cf7900efbdad0b3ee83eba7
Divjason <126494515+Divjason@users.noreply.github.com>
2023-08-10 15:21:41 +0900

    Add files via upload

---
+++ 1.다운로드/coin.tsx
@@ -0,0 +1,29 @@
+[
+  {
+    id: 'btc-bitcoin',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'eth-ethereum',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'hex-hex',
+    name: 'HEX',
+    symbol: 'HEX',
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: 'token',
+  },
+];
